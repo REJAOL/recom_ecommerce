31a0bd6629cbee85a3b718e9af0aa6bdbdc0f4fc
REJAOL <karimsalahuddin03@gmail.com>
2024-06-08 22:27:43 +0600

    added owner creation method and configure development environment

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,13 +1,16 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const config = require("config");
+
+const dbgr = require("debug")("development:mongoose");
 
 mongoose
-.connect("mongodb://localhost:27017/recom")
-.then(function(){
-    console.log("connected");
-})
-.catch(function(err){
-    console.log(err);
-})
+    .connect(`${config.get("MONGODB_URI")}/recom`)
+    .then(function () {
+        dbgr("connected");
+    })
+    .catch(function (err) {
+        dbgr(err);
+    })
 
 
 module.exports = mongoose.connection
--- routes/ownerRoute.js
+++ routes/ownerRoute.js
@@ -1,9 +1,34 @@
 const express = require("express")
 
 const router = express.Router()
+const ownerModel = require("../models/owner-model.js")
+
+
+if(process.env.NODE_ENV === "development"){
+    router.post('/create', async function(req,res){
+        let owners = await ownerModel.find()
+        if(owners.length>0){
+            return res
+                .send(503)
+                .send("you don't have permission to create another owner")
+        }
+        let {fullname,email,password} = req.body
+        let createdOwner = await ownerModel.create({
+            fullname,
+            email,
+            password
+        })
+        res.status(201).send(createdOwner)
+    })
+}
 
 router.get("/", function(req,res){
     res.send("hello from owner page")
 })
+
+
+
+
+
 
 module.exports = router
